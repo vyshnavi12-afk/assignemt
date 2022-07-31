3079872151e845eb04f21f46b7224d1d379e862f
vyshnavi12-akf <gvyshnavi2003@gmail.com>
2022-07-31 05:12:25 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css'
+
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
+
+const App = () => {
+    <div className="appcontaier">
+    <ul>
+   
+   for(let i;i<bannerCardsList.length;i++){
+
+<bannercarditem key={bannerCardsList[i].id} headerText={bannerCardsList[i].headerText} description={bannerCardsList[i].description} className={bannerCardsList[i].className}/>
+  
+   }
+   </ul>
+   </div>
+   
+}
+export default App
+
---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,10 @@
+// Write your code here.
+let bannercarditem=(props)=>{
+     return(
+        const {id,headerText,description,className}=props;
+        <li className={className}>
+            <h1>{headerText}</h1>
+            <p>{description}</p>
+        </li>
+)
+     }
